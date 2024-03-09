c802fafe971c4c4f3e636d8c92cd39de5e14a6b3
Joel Kuzmarski <leoj3n@gmail.com>
2024-03-09 03:39:20 -0600

    Simplify

--- src/lib/MyButton.svelte.d.ts
+++ src/lib/MyButton.svelte.d.ts
@@ -17,41 +17,11 @@ export type MyButtonEvents = typeof __propDef.events;
 export type MyButtonSlots = typeof __propDef.slots;
 
 /**
+ * ## MyButton
  * Here's some documentation for this component.
- *
- * ## Usage
- * - Simple usage:
- * ```svelte
- * <MyButton>Foo</MyButton>
- * ```
- * - Advanced usage: 
- * ```svelte
- * <MyButton myName="Foo" myCustomProp={true}>Bar</MyButton>
- * ```
- *
- * ## Props
+ * ### Props
  * @prop export let myCustomProp: boolean;
  * @prop export let myName: string;
  */
 export default class MyButton extends SvelteComponent<MyButtonProps, MyButtonEvents, MyButtonSlots> {}
 export {};
-
-
-// import type { HTMLButtonAttributes } from 'svelte/elements';
-
-// export default interface $$Props extends HTMLButtonAttributes {
-// 	myName: string;
-// 	myCustomProp: boolean;
-// }
-
-
-// import type { SvelteComponent } from 'svelte';
-// import type { SvelteHTMLElements } from 'svelte/elements';
-
-// type RestProps = SvelteHTMLElements['button'];
-// interface Props extends RestProps {
-// 	myName: string;
-// 	myCustomProp: boolean;
-// }
-
-// export default class MyButton extends SvelteComponent<Props> {}
---
+++ src/lib/ambient.d.ts
@@ -0,0 +1,21 @@
+// @TODO: Decide if these patterns are no longer needed... 
+
+
+// import type { HTMLButtonAttributes } from 'svelte/elements';
+
+// export default interface $$Props extends HTMLButtonAttributes {
+// 	myName: string;
+// 	myCustomProp: boolean;
+// }
+
+
+// import type { SvelteComponent } from 'svelte';
+// import type { SvelteHTMLElements } from 'svelte/elements';
+
+// type RestProps = SvelteHTMLElements['button'];
+// interface Props extends RestProps {
+// 	myName: string;
+// 	myCustomProp: boolean;
+// }
+
+// export default class MyButton extends SvelteComponent<Props> {}
