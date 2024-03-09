3ba0435a32f94f76710ef49c4a9a489b520c05eb
Joel Kuzmarski <leoj3n@gmail.com>
2024-03-09 01:11:47 -0600

    Will try publish

--- src/lib/MyButton.svelte
+++ src/lib/MyButton.svelte
@@ -25,12 +25,19 @@
 Here's some documentation for this component.
 It will show up on hover.
 
+## Props
+
+@prop export let myName: string;
+@prop export let myCustomProp: boolean;
+
+## Usage
+
 - Simple usage:
   ```tsx
   <MyButton>Foo</MyButton>
   ```
 - Advanced usage:
   ```tsx
-  <MyButton myName="Foo" myCustomProp={true}>Baz</MyButton>
+  <MyButton myName="Foo" myCustomProp={true}>Bar</MyButton>
   ```
 -->
